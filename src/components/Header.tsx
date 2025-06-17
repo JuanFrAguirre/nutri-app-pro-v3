9a9712dd26eec8e420aabcb1269f43cdab5f2aac
Juan Francisco Aguirre <jf_aguirre@hotmail.com>
2025-06-17 06:31:38 +0200

    fix: PWA pb fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -60,7 +60,7 @@ const MobileNav = ({ links, pathname }: NavProps) => {
     setIsMenuOpen(false);
   };
   return (
-    <header className="fixed bottom-0 md:hidden inset-x-0 bg-brand-whiter shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1),0_-2px_4px_-2px_rgba(0,0,0,0.1)] h-12 md:h-16 flex items-center">
+    <header className="fixed bottom-0 md:hidden inset-x-0 bg-brand-whiter shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1),0_-2px_4px_-2px_rgba(0,0,0,0.1)] h-12 md:h-16 flex items-center header-mobile pb-safe">
       <div
         className={clsx(
           'container mx-auto flex items-center',
